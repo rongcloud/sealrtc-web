4365ec7ad4b6704b1b2e34f041cdb08b3b3af1d4
guo wei kai <guoweikai@bailingcloud.com>
2018-09-07 14:19:49 +0800

    增加config配置文件

---
+++ config.js
@@ -0,0 +1,7 @@
+var global_config = {
+        TOKEN_URL : 'https://rtc.ronghub.com/voiptoken',
+        WS_NAV_URL : 'https://rtc.ronghub.com/nav/websocketlist',
+        DOWNLOAD_SHARE_PLUGIN_URL:'https://rtc.ronghub.com/plugin/chrome-addon.zip',
+        IS_SHOW_WB : true,
+        appId:'x4vkb1qpxfrzk'
+}
